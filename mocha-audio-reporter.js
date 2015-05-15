a87ee0da972b5c0e68428d715d2c6cefad51763c
CoyoteBringsFire <aumkara@gmail.com>
2015-05-15 11:07:47 -0700

    refactored to use lame/speaker instead of singer

--- mocha-audio-reporter.js
+++ mocha-audio-reporter.js
@@ -1,6 +1,7 @@
 "use strict";
 var debug=require('debug')('mocha:audioreporter'),
-    Singer = require('singer');
+    lame=require('lame'),
+    Speaker=require('Speaker');
 
 module.exports = mocha_audio_reporter;
 
@@ -13,23 +14,37 @@ function mocha_audio_reporter(runner, options) {
   }
   debug("%j", options);
 
-  self.singer = new Singer();
+  self.speak=function(file) {
+    var speaker=new Speaker({
+      channels: 2,          // 2 channels 
+      bitDepth: 16,         // 16-bit samples 
+      sampleRate: 44100     // 44,100 Hz sample rate 
+    });
+    
+    fs.createReadStream(file)
+      .pipe(new lame.Decoder)
+      .pipe(speaker);
+    return speaker;
+  };
 
-  runner.on('start', function onStart(test) {
-    // start playing music
-    self.singer.sing( options.onStart || "" );
-    self.singer.on('singing', function() {
-      debug("singing still");
-    });
-    self.singer.on('end', function onEnd() {
-      debug("done singing");
-    });
-  });
+  runner.on('start', function onStart(test) {});
 
   runner.on('pass', function onTestPass(test){
     // play a happy tone
     if( options.onPass ) {
-      self.singer.sing( options.onPass );
+      self.speak( options.onPass )
+        .on('close', function speakerClosed() {
+          debug("done playing");
+        });
+    }
+    if( options.onPass === undefined ) {
+      self.speak( __dirname+'/audio/testPass.mp3' )
+        .on('close', function speakerClosed() {
+          debug("done playing");
+        });
+    }
+    if( options.onPass === null ) {
+      debug("Not playing a sound");
     }
   });
 
@@ -37,19 +52,57 @@ function mocha_audio_reporter(runner, options) {
     failures++;
     // play a sad tone
     if( options.onFail ) {
-      self.singer.sing( options.onFail );
+      self.speak( options.onFail )
+        .on('close', function speakerClosed() {
+          debug("done playing");
+        });
+    }
+    if( options.onFail === undefined ) {
+      self.speak( __dirname+'/audio/testFail.mp3' )
+        .on('close', function speakerClosed() {
+          debug("done playing");
+        });
+    }
+    if( options.onFail === null ) {
+      debug("Not playing a sound");
     }
   });
 
   runner.on('end', function onTestEnd(){
     debug("runner.end");
-    // stop playing music
-    self.singer.stop();
-    if( failures && options.onSuiteFail ) {
-      self.singer.sing( options.onSuiteFail );
-    } else  if( options.onSuitePass ) {
-      self.singer.sing( options.onSuitePass );
+    
+    if( failures ) {
+      debug("suite failed %j", options);
+      if( options.onSuiteFail ) {
+        debug('playing %s', options.onSuiteFail );
+        self.speak( options.onSuiteFail )
+          .on('close', function speakerClosed() {
+            debug("done playing");
+          });
+      }
+      if( options.onSuiteFail === undefined ) {
+        debug('playing %s', __dirname+'/audio/suiteFail.mp3');
+        self.speak( __dirname+'/audio/suiteFail.mp3' )
+          .on('close', function speakerClosed() {
+            debug("done playing");
+          });
+      }
+    } else {
+      debug("suite passed %j", options);
+      if( options.onSuitePass ) {
+        debug('playing %s', options.onSuitePass );
+        self.speak( options.onSuitePass )
+          .on('close', function speakerClosed() {
+            debug("done playing");
+          });
+      }
+      if( options.onSuitePass === undefined ) {
+        debug('playing %s', __dirname+'/audio/suitePass.mp3');
+        self.speak( __dirname+'/audio/suitePass.mp3' )
+          .on('close', function speakerClosed() {
+            debug("done playing");
+          });
+      }
     }
   });
-
 }
