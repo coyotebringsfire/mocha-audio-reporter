e10d99f6b3976628a2e916a62cb001a1fdca37ff
CoyoteBringsFire <aumkara@gmail.com>
2015-05-14 17:54:53 -0700

    refactored for singer events and made songs for each event optional

--- mocha-audio-reporter.js
+++ mocha-audio-reporter.js
@@ -7,52 +7,50 @@ module.exports = mocha_audio_reporter;
 
 function mocha_audio_reporter(runner, options) {
   var failures=0,
-      player, stopTest=false;
+      self=this, stopTest=false;
 
   if(!(this instanceof mocha_audio_reporter)) {
     return new mocha_audio_reporter(runner, options);
   }
+  debug("%j", options);
+
+  self.singer = new Singer();
 
   runner.on('start', function onStart(test) {
-    function onDonePlaying(err, player) {
-      debug('playend!');
-      currentSong=player.play(onDonePlaying);
-    }
     // start playing music
-    singer = new Singer();
-    // play now and callback when playend
-    var currentSong=singer.sing();
-    currentSong.on('playing', function onPlaying() {
-      debug("playing");
-    })
-    currentSong.on('done', onDonePlaying);
-    currentSong.on('stopped', function onStopped() {
-      debug("stopped");    
+    self.singer.sing( options.onStart || "" );
+    self.singer.on('singing', function() {
+      debug("singing still");
+    });
+    self.singer.on('end', function onEnd() {
+      debug("done singing");
     });
   });
 
   runner.on('pass', function onTestPass(test){
     // play a happy tone
-    singer = new Player();
-    singer.sing(sounds["smb_coin"]);
+    if( options.onPass ) {
+      self.singer.sing( options.onPass );
+    }
   });
 
   runner.on('fail', function onTestFail(test, err){
     failures++;
     // play a sad tone
-    singer = new Player();
-    singer.sing(sounds["smb_mariodie"]);
+    if( options.onFail ) {
+      self.singer.sing( options.onFail );
+    }
   });
 
   runner.on('end', function onTestEnd(){
     debug("runner.end");
     // stop playing music
-    singer.stop();
-    singer= new Singer();
-    if( failures ) {
-      singer.sing(sounds["smb_gameover"]);
-    } else {
-      singer.sing(sounds["smb_stage_clear"]);
+    self.singer.stop();
+    if( failures && options.onSuiteFail ) {
+      self.singer.sing( options.onSuiteFail );
+    } else  if( options.onSuitePass ) {
+      self.singer.sing( options.onSuitePass );
     }
   });
+
 }
